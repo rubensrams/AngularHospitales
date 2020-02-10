b7f4898791d45d9f7536a4b3ff474aaf519d0658
rubensrams <you@example.com>
2020-02-09 21:57:20 -0600

    Fin de la seccion 7

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+declare function _initPlugins();
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(public _router: Router) { }
 
   ngOnInit() {
+    _initPlugins();
+
   }
+
+ingresar(){
+
+  this._router.navigate(['/dashboard']);
+}
 
 }
